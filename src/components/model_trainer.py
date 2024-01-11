ea8ed62d669cfd9268f1beb98898dcafb90ea06c
SreehariNvklm <shnvklm2004@gmail.com>
2024-01-11 19:24:42 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -5,7 +5,6 @@ from dataclasses import dataclass
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.neighbors import KNeighborsRegressor
 
 from sklearn.metrics import r2_score
 
@@ -34,11 +33,25 @@ class ModelTrainer:
                 "Random Forest" : RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Linear Regression" : LinearRegression(),
-                "K-Neighbors Regression" : KNeighborsRegressor(),
-                "Adaboost Regression" : AdaBoostRegressor()
+                "AdaBoost Regressor" : AdaBoostRegressor()
+            }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest":{
+                   
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
 
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
